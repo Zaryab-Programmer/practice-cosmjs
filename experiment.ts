885440567d31e96614d5c68bee6060a3235ddbc8
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2023-03-24 16:29:45 +0500

    first commit

---
+++ experiment.ts
@@ -0,0 +1,10 @@
+import { StargateClient } from "@cosmjs/stargate";
+
+const rpc = "rpc.sentry-01.theta-testnet.polypore.xyz:26657";
+
+const runAll = async(): Promise<void> => {
+    const client = await StargateClient.connect(rpc);
+    console.log("With client, chain id:", await client.getChainId(), ", height:", await client.getHeight())
+}
+
+runAll()
